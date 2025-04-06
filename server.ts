3a56e51580c36ad05e3cc8408c026c6e912cb698
LosOleksandr <losalexander2003@gmail.com>
2025-04-06 18:05:50 +0300

    feat: setup postgres db with pg-promise package

--- server.ts
+++ server.ts
@@ -1,1 +1,21 @@
-console.log('Hello via Bun!')
+import db from '@/db'
+import env from '@utils/env'
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    db.any('SELECT * FROM movies')
+        .then((movies) => {
+            return res.json(movies)
+        })
+        .catch((err) => {
+            console.error('Error fetching movies:', err)
+            res.status(500).send('Error fetching movies')
+        })
+})
+
+app.listen(env.PORT, () => {
+    console.log(`Listening on port ${env.PORT}...`)
+})
