07aaa4a849d80df37d41376b9b22ef248c239fb3
LosOleksandr <losalexander2003@gmail.com>
2025-04-06 16:51:46 +0300

    init: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tseslintParser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+    eslint.configs.recommended,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tseslintParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            'prettier/prettier': 'error',
+        },
+    },
+    prettier,
+]
