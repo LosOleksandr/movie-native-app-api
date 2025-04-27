5ab0fb5264e15d80e50ce1694bb813064bbae865
LosOleksandr <losalexander2003@gmail.com>
2025-04-27 14:43:58 +0300

    fix: fix returning type in register and login

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ app.use(router)
 
 app.use(
     cors({
-        origin: env.FRONTEND_URL,
+        origin: true,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
